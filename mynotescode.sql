0564d7a620652df18375b602979969cf19362b06
Andar Pratama <master.amarta@gmail.com>
2020-11-17 19:38:43 +0700

    add sql

--- mynotescode.sql
+++ mynotescode.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.2
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 06, 2018 at 05:30 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.8
+-- Host: localhost:3306
+-- Generation Time: Nov 17, 2020 at 12:37 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -30,23 +28,26 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `siswa` (
   `id` int(11) NOT NULL,
-  `nis` varchar(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `jenis_kelamin` varchar(10) NOT NULL,
-  `telp` varchar(15) NOT NULL,
+  `nama` varchar(128) NOT NULL,
+  `nis` varchar(128) NOT NULL,
+  `jenis_kelamin` varchar(128) NOT NULL,
+  `gender` varchar(128) NOT NULL,
+  `telp` varchar(12) NOT NULL,
   `alamat` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `siswa`
 --
 
-INSERT INTO `siswa` (`id`, `nis`, `nama`, `jenis_kelamin`, `telp`, `alamat`) VALUES
-(1, '1104070001', 'Rizaldi Maulidia Achmad', 'Laki-laki', '089277182122', 'Sumedang'),
-(2, '1104070002', 'Siska Melina Rachman', 'Perempuan', '081262551722', 'Bandung'),
-(3, '1104070003', 'Asep Dadan', 'Laki-laki', '085162718881', 'Garut'),
-(6, '1104070004', 'Samba Ilham', 'Laki-laki', '08918817726', 'Bandung'),
-(7, '1104070005', 'Nadya Nur Syahbandi', 'Perempuan', '081255171821', 'Jakarta');
+INSERT INTO `siswa` (`id`, `nama`, `nis`, `jenis_kelamin`, `gender`, `telp`, `alamat`) VALUES
+(2, 'Andar', '3545245', 'Perempuan', '', '4354542', 'fgdfdfhd'),
+(3, 'fsdfsdfds', '334534', 'Perempuan', '', '35443', 'dggdg'),
+(4, 'ghghfg', '4545', 'Perempuan', '', '544545', 'hfghghfgh'),
+(5, 'andar', '32323', 'Laki-laki', '', '2003033', 'fgfdgfd'),
+(6, 'gdf', '434532542', 'Laki-laki', '', '542525252', 'fgdgdfdgdfgdff'),
+(7, 'fdfsf', '34334', 'Perempuan', '', '322432', 'sffsfsdfdsfdsfdsfsd'),
+(8, 'fdfd', '4848484', 'Perempuan', '', '44949494', 'ads');
 
 --
 -- Indexes for dumped tables
@@ -66,8 +67,7 @@ ALTER TABLE `siswa`
 -- AUTO_INCREMENT for table `siswa`
 --
 ALTER TABLE `siswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
